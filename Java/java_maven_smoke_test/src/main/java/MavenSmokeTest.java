1709a5a75aded5eef006671a705fb81804b3015e
Dileepkumar A V <linkdileep@gmail.com>
2026-06-05 13:37:13 +0530

    Java environment validation

---
+++ Java/java_maven_smoke_test/src/main/java/MavenSmokeTest.java
@@ -0,0 +1,9 @@
+public class MavenSmokeTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Maven Environment Working");
+
+    }
+
+}
---
+++ Java/java_smoke_test/src/JavaSmokeTest.java
@@ -0,0 +1,9 @@
+public class JavaSmokeTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Environment Working");
+
+    }
+
+}
