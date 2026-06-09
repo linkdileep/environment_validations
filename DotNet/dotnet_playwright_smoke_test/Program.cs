807ec878c7c785bff4b501d98fad6b4da5ebc04f
Dileepkumar A V <linkdileep@gmail.com>
2026-06-09 18:51:54 +0530

    Playwright validation

---
+++ DotNet/dotnet_playwright_smoke_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+
+await using var browser = await playwright.Chromium.LaunchAsync(
+    new BrowserTypeLaunchOptions
+    {
+        Headless = true
+    });
+
+Console.WriteLine("Playwright Browser Launch Successful");
+
+await browser.CloseAsync();
